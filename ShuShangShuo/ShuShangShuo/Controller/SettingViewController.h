a6848130cb47f3c855da4472a061ddeb67ce8f2c
张泽瀛 <zhangzy@weipaitang.com>
2018-06-04 23:35:41 +0800

    1.0.0——增加设置界面

---
+++ ShuShangShuo/ShuShangShuo/Controller/SettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.h
+//  ShuShangShuo
+//
+//  Created by zhangzey on 04/06/2018.
+//  Copyright © 2018 lanmao. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface SettingViewController : BaseViewController
+
+@end
---
+++ ShuShangShuo/ShuShangShuo/Controller/SettingViewController.m
@@ -0,0 +1,27 @@
+//
+//  SettingViewController.m
+//  ShuShangShuo
+//
+//  Created by zhangzey on 04/06/2018.
+//  Copyright © 2018 lanmao. All rights reserved.
+//
+
+#import "SettingViewController.h"
+
+@interface SettingViewController ()
+
+@end
+
+@implementation SettingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+@end
