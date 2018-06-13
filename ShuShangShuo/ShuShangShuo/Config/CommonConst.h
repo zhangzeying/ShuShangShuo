0c95a47166d71c194ab028829eb3089658fc51bc
张泽瀛 <zhangzy@weipaitang.com>
2018-06-13 22:20:06 +0800

    1.0.0——一些宏定义

--- ShuShangShuo/ShuShangShuo/Config/CommonConst.h
+++ ShuShangShuo/ShuShangShuo/Config/CommonConst.h
@@ -14,6 +14,8 @@
 #define ScreenWidth ScreenBounds.size.width
 #define ScreenHeight ScreenBounds.size.height
 
+#define kWindow ([UIApplication sharedApplication].keyWindow)
+
 #define iPhone4_inch (ScreenHeight <= 480)
 #define iPhone5_inch (ScreenHeight > 480 && ScreenHeight <= 568)
 #define iPhone6_inch (ScreenHeight > 568 && ScreenHeight <= 667)
@@ -35,6 +37,23 @@
 #define kTopHeight ([[UIApplication sharedApplication] statusBarFrame].size.height>20?44:20)
 
 //当前App的版本号
-#define SCurrentVersion [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"]
+#define KCurrentVersion [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"]
+
+#define kDocuments NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES).firstObject
+
+//---- epub相关 ----
+#define TopSpacing 40.0f
+#define BottomSpacing 40.0f
+#define LeftSpacing 20.0f
+#define RightSpacing  20.0f
+#define MinFontSize 11.0f
+#define MaxFontSize 20.0f
+#define RGB(R, G, B) [UIColor colorWithRed:R/255.0 green:G/255.0 blue:B/255.0 alpha:1.0]
+#define DELAYEXECUTE(delayTime,func) (dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayTime * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{func;}))
+#define DistanceFromTopGuiden(view) (view.frame.origin.y + view.frame.size.height)
+#define DistanceFromLeftGuiden(view) (view.frame.origin.x + view.frame.size.width)
+#define ViewOrigin(view)   (view.frame.origin)
+
+#define kUserDefaults [NSUserDefaults standardUserDefaults]
 
 #endif /* CommonConst_h */
--- ShuShangShuo/ShuShangShuo/Config/SaticCommonConst.h
+++ ShuShangShuo/ShuShangShuo/Config/SaticCommonConst.h
@@ -12,4 +12,24 @@
 //---- FileName ----
 #define kSearchHistoryFileName @"SearchHistoryFileName"
 
+//---- epub file ----
+// 缓存主目录
+#define HSCachesDirectory [[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:@"HSCache"]
+// 保存文件名
+#define HSFileName(url) url.md5String
+// 文件的存放路径（caches）
+#define HSFileFullpath(url) [HSCachesDirectory stringByAppendingPathComponent:HSFileName(url)]
+// 文件的已下载长度
+#define HSDownloadLength(url) [[[NSFileManager defaultManager] attributesOfItemAtPath:HSFileFullpath(url) error:nil][NSFileSize] integerValue]
+
+// 存储文件总长度的文件路径（caches）
+#define HSTotalLengthFullpath [HSCachesDirectory stringByAppendingPathComponent:@"totalLength.plist"]
+
+//---- Notification ----
+#define LSYNoteNotification @"LSYNoteNotification"
+#define LSYThemeNotification @"LSYThemeNotification"
+#define LSYEditingNotification @"LSYEditingNotification"
+#define LSYEndEditNotification @"LSYEndEditNotification"
+
+
 #endif /* SaticCommonConst_h */
